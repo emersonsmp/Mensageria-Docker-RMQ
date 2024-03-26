109e9bdf3d1ad4302f698273bc1caeeaf3fe6389
Emerson Sampaio <32761815+emersonsmp@users.noreply.github.com>
2024-03-26 18:52:58 -0300

    Sender service
    Sender service

---
+++ Send/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "lead-users-list",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: true,
+                     arguments: null);
+
+Console.WriteLine(value: "type a message and press <ENTER>");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "") break;
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: "lead-users-list",
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine(value: $" [x] Message -> {message}, Sent!");
+}
